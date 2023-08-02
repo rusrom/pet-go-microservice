60be0a5dc9721dcc7285e73c6054ad9087e998c5
rusrom <19729227+rusrom@users.noreply.github.com>
2023-08-02 14:36:44 +0300

    front end connected to broker service

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -7,13 +7,15 @@ import (
 	"net/http"
 )
 
+const port = "8080"
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		render(w, "test.page.gohtml")
 	})
 
-	fmt.Println("Starting front end service on port 80")
-	err := http.ListenAndServe(":8080", nil)
+	fmt.Println("Starting front end service on http://localhost:" + port)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Panic(err)
 	}
